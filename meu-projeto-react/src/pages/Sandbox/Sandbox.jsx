ff2051cb41b86d1f0747f42a24b4c1e9e98b7586
andressa-barros <andressa.barros4194@gmail.com>
2026-05-26 20:16:16 -0300

    Create Footer Component View

--- meu-projeto-react/src/pages/Sandbox/Sandbox.jsx
+++ meu-projeto-react/src/pages/Sandbox/Sandbox.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import Header from '../../components/Header/Header';
-import Filters from '../../components/Filters/Filters';
-import './Sandbox.css';
+import React from "react";
+import Header from "../../components/Header/Header";
+import Filters from "../../components/Filters/Filters";
+import Footer from "../../components/Footer/Footer";
+import "./Sandbox.css";
 
 const Sandbox = () => {
   return (
     <div className="sandbox-container">
       <Header />
-      
+
       <main className="sandbox-layout">
         <Filters />
-        
+
         <div className="sandbox-content">
           {/* Espaço para futuros componentes como cards de pets */}
-          <h2 style={{ padding: '40px', color: '#888' }}>
-            Main Content Area
-          </h2>
+          <h2 style={{ padding: "40px", color: "#888" }}>Main Content Area</h2>
         </div>
       </main>
+
+      <Footer />
     </div>
   );
 };
